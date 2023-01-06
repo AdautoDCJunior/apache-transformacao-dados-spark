3fc6ad25fffd8f897be9759d94169225ef31c198
Adauto Júnior <adautodcjunior@gmail.com>
2023-01-06 16:07:27 -0300

    finishing class 03

--- airflow/dags/twitter_dag.py
+++ airflow/dags/twitter_dag.py
@@ -3,8 +3,8 @@ sys.path.append('airflow')
 
 from airflow.models import DAG
 from operators.twitter_operator import TwitterOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from airflow.utils.dates import days_ago
-from datetime import datetime, timedelta
 from os.path import join
 
 query = "datascience"
@@ -13,15 +13,31 @@ with DAG(
   start_date=days_ago(6),
   schedule_interval='0 0 * * *'
 ) as dag:
-    to = TwitterOperator(
-      task_id='twitter_datascience',
-      file_path=join(
-        'datalake',
-        'twitter_datascience',
-        f'extract_date={{{{ ds }}}}',
-        f'datascience_{{{{ ds_nodash }}}}.json'
-      ),
-      start_time='{{ data_interval_start.strftime("%Y-%m-%dT%H:%M:%S.00Z") }}',
-      end_time='{{ data_interval_end.strftime("%Y-%m-%dT%H:%M:%S.00Z") }}',
-      query=query
-    )
+  twitter_operator = TwitterOperator(
+    task_id='twitter_datascience',
+    file_path=join(
+      'datalake',
+      'twitter_datascience',
+      f'extract_date={{{{ ds }}}}',
+      f'datascience_{{{{ ds_nodash }}}}.json'
+    ),
+    start_time='{{ data_interval_start.strftime("%Y-%m-%dT%H:%M:%S.00Z") }}',
+    end_time='{{ data_interval_end.strftime("%Y-%m-%dT%H:%M:%S.00Z") }}',
+    query=query
+  )
+
+  twitter_transform = SparkSubmitOperator(
+    task_id='transform_twitter_datascience',
+    application='/home/adauto_junior/cursos/alura/airflow-transformacao-dados-spark/src/scripts/transformation.py',
+    name='twitter_transformation',
+    application_args=[
+      '--src', 
+      '/home/adauto_junior/cursos/alura/airflow-transformacao-dados-spark/datalake/twitter_datascience', 
+      '--dst',
+      '/home/adauto_junior/cursos/alura/airflow-transformacao-dados-spark/transform_data',
+      '--process-date',
+      '{{ ds }}'
+    ]
+  )
+
+twitter_operator >> twitter_transform
